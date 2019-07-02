5fd1aca3d796749351ca2f4d3c74e2850942a08e
Jakov Krunić <36998346+jakovkrunic@users.noreply.github.com>
2019-07-03 00:25:11 +0200

    popravljen bug u accountservice

--- model/accountservice.class.php
+++ model/accountservice.class.php
@@ -21,13 +21,13 @@ class AccountService
 											$row['valuta_racuna'], $row['stanje_racuna'],	$row['datum_izrade'], $row['odobren'], $row['dozvoljeni_minus']);
 	}
 
-	function getAllUserAccounts( $user_oib )		 //pronadi sve racune korisnika ciji je oib $user_oib
+	function getAllUserAccounts( $user_oib )		 //pronadi sve odobrene racune korisnika ciji je oib $user_oib
 	{
 		try
 		{
 			$db = DB::getConnection();
-			$st = $db->prepare( 'SELECT * FROM projekt_racun WHERE oib=:oib' );
-			$st->execute( array( 'oib' => $user_oib) );
+			$st = $db->prepare( 'SELECT * FROM projekt_racun WHERE oib=:oib AND odobren=:odobren' );
+			$st->execute( array( 'oib' => $user_oib , 'odobren' => 1) );
 		}
 		catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
 
@@ -46,8 +46,8 @@ class AccountService
 		try
 		{
 			$db = DB::getConnection();
-			$st = $db->prepare( 
-			'INSERT INTO projekt_racun (oib,tip_racuna,valuta_racuna,stanje_racuna,dozvoljeni_minus, datum_izrade, odobren) 
+			$st = $db->prepare(
+			'INSERT INTO projekt_racun (oib,tip_racuna,valuta_racuna,stanje_racuna,dozvoljeni_minus, datum_izrade, odobren)
 			VALUES (:user_oib, :vrsta_racuna, :valuta_racuna, 0, :zeljeni_minus, :datum, 0);');
 			$st->execute( array( 'user_oib' => $user_oib, 'vrsta_racuna' => $vrsta_racuna, 'valuta_racuna' => $valuta_racuna, 'zeljeni_minus' => $zeljeni_minus , 'datum' => date('Y-m-d')) );
 		}
