28bfcd286b97fa64b1cab92fcb6914d93731fd4f
gumarin1 <37241245+gumarin1@users.noreply.github.com>
2019-07-06 03:11:47 +0200

    Poboljsan user index

--- js/tecaj.js
+++ js/tecaj.js
@@ -12,15 +12,12 @@ function(data, status) {
       var tr = document.createElement("tr");
       var th = document.createElement("th");
       th.innerHTML = key;
-      th.style.border = "1px solid black";
 
       var td1 = document.createElement("td");
       td1.innerHTML = data.rates[key];
-      td1.style.border = "1px solid black";
 
       var td2 = document.createElement("td");
       td2.innerHTML = (1 / parseFloat(data.rates[key])).toFixed(10);
-      td2.style.border = "1px solid black";
 
       tr.appendChild(th);
       tr.appendChild(td1);
--- view/user_index.php
+++ view/user_index.php
@@ -1,24 +1,28 @@
 <?php require_once 'view/_header.php';
 ?>
 <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
-<h1> Karta </h1>
-<div id="map" class="map"></div>
-<script src="<?php echo __SITE_URL;?>/js/mapa.js"></script>
 
-<h1> Tečajna lista </h1>
-<table id="table_exchange" style="border: 1px solid black">
+<h2> Tečajna lista </h2>
+<table id="table_exchange" align="center">
   <tr>
-  <th style="border: 1px solid black">*</th>
-  <th style="border: 1px solid black"> HRK / * </th>
-  <th style="border: 1px solid black"> * / HRK </th>
+  <th>*</th>
+  <th> HRK / * </th>
+  <th> * / HRK </th>
 
 </tr>
 </table>
 <br>
+<script src="<?php echo __SITE_URL;?>/js/tecaj.js"></script>
+<h2> Karta </h2>
+<div id="map" class="map" align="center"></div>
+<script src="<?php echo __SITE_URL;?>/js/mapa.js"></script>
+
+<br>
+
 <h1>Kontakt informacije</h1>
 Za sve informacije ili nedoumice, navratite u jednu od naših poslovnica ili nas kontaktirajte putem telefona ili e-maila.<br>
 Broj telefona: +385 (0)1 4605 777<br>
 E-mail: morz(at)math.hr
-<script src="<?php echo __SITE_URL;?>/js/tecaj.js"></script>
+
 <?php
 require_once 'view/_footer.php'; ?>
